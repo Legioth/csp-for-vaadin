c347f23df60174ccd3c8dd3ea2a33cc45905069c
Leif Åstrand <leif@vaadin.com>
2021-05-14 13:55:30 +0300

    Add a description to the demo view

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,5 +1,7 @@
 package com.example.application.views.helloworld;
 
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -12,13 +14,18 @@ import com.vaadin.flow.router.RouteAlias;
 
 @Route(value = "hello", layout = MainView.class)
 @RouteAlias(value = "", layout = MainView.class)
-@PageTitle("Hello World")
+@PageTitle("Hello World with CSP")
 public class HelloWorldView extends HorizontalLayout {
 
     private TextField name;
     private Button sayHello;
 
     public HelloWorldView() {
+        add(new Html(
+                "<p>This application shows how Vaadin 14 can work with a Content Security Policy setting that disallows evaluating arbitrary JavaScript. "
+                        + "There is nothing special with this view in itself, other than that it is fully functional even with the <code>Content-Security-Policy</code> header in the HTML response. "
+                        + "<br>See <a href='https://vaadin.com/labs/csp'>the description in Vaadin Labs</a> for more information.</p>"));
+
         addClassName("hello-world-view");
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
